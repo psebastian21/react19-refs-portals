4ae8172c1ca24a03fffbc8779d9ff5dce55d1386
Paulo Sebastián García Vargas <psebastian21@gmail.com>
2025-06-26 23:49:15 -0500

    Storing name with useState

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,23 @@
+import {useState} from "react";
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+    const [nameInputValue, setNameInputValue] = useState('');
+    const [buttonClicked, setButtonClicked] = useState(false);
+
+    const handleNameInputChange = (event) => {
+        setButtonClicked(false);
+        setNameInputValue(event.target.value);
+    }
+    const handleButtonClick = () => {
+        setButtonClicked(true);
+    }
+    return (
+        <section id="player">
+            <h2>Welcome {buttonClicked ? nameInputValue : "unknown entity"} </h2>
+            <p>
+                <input type="text" onChange={handleNameInputChange} value={nameInputValue}/>
+                <button onClick={handleButtonClick}>Set Name</button>
+            </p>
+        </section>
+    );
 }
